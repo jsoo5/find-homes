72b0e12a30483f92ca93f99d8950bf2cd66ba930
jisooOh <122949955+jsoo5@users.noreply.github.com>
2025-04-15 01:31:25 +0900

    Update app.py
    Edit port number to 8050

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def process_request(user_input, callback_url, source_filter, user_id):
 if __name__ == "__main__":
     if os.getenv("AZURE_APP_SERVICE"):
         # Azure 배포용
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
     else:
         # 로컬 개발용
         app.run(debug=True)
