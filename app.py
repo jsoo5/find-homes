13fb6916a966382921417f4f52465036dafc5142
jisooOh <122949955+jsoo5@users.noreply.github.com>
2025-04-16 05:23:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -124,10 +124,11 @@ def process_request(user_input, callback_url, source_filter, user_id):
         print("❌ Callback 전송 실패:", e)
 
 if __name__ == "__main__":
-    if os.getenv("AZURE_APP_SERVICE"):
-        # Azure 배포용
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
-    else:
-        # 로컬 개발용
-        app.run(debug=True, port=5000)
-    #     app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    # if os.getenv("AZURE_APP_SERVICE"):
+    #     # Azure 배포용
+    #     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    # else:
+    #     # 로컬 개발용
+    #     app.run(debug=True, port=5000)
+    # #     app.run(debug=True)
